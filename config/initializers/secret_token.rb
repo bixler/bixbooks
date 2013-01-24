9da969a03fcec9b7862912ea366226a6acaf7fe4
James Bixler <james.bixler@boomtime.com>
2013-01-23 21:50:34 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bixbooks::Application.config.secret_token = '117e00ccc7e43e914de695ca9220cda6b98be1213013d0fe867f510c65fe0230150467ca496415a57b9d37a162ae6d1949ac91954e921b6c9ef53e9e87c3a3fc'
